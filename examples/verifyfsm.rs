00bd63ee12bd3c1dbc332f810881dcf8bae78bc0
Andrew Yourtchenko <ayourtch@gmail.com>
2025-01-18 23:55:25 +0100

    fix crash when not able to load yaml

--- examples/verifyfsm.rs
+++ examples/verifyfsm.rs
@@ -21,43 +21,49 @@ fn main() {
         .expect("Missing TextFSM verify data YAML file name");
     let mut textfsm = TextFSM::from_file(&template_name);
     let yaml = std::fs::read_to_string(&yaml_verify_name).expect("YAML File read failed");
-    let yaml_map: ParsedSample = serde_yaml::from_str(&yaml).expect("YAML deserialize failed");
-
     let result = textfsm.parse_file(&data_name, Some(DataRecordConversion::LowercaseKeys));
     println!("RESULT: {:?}\n", &result);
-    if result == yaml_map.parsed_sample {
-        println!("Parsed result matches YAML");
+
+    if let Ok(yaml_map) = serde_yaml::from_str::<ParsedSample>(&yaml)
+    {
+        if result == yaml_map.parsed_sample {
+            println!("Parsed result matches YAML");
+        } else {
+            println!("Results differ");
+            println!("Records: {:?}", &result);
+            println!("\n");
+            println!("yaml: {:?}", &yaml_map.parsed_sample);
+            println!("\n");
+
+            let mut only_in_yaml: Vec<Vec<String>> = vec![];
+            let mut only_in_parse: Vec<Vec<String>> = vec![];
+
+            for (i, irec) in result.iter().enumerate() {
+                let mut vo: Vec<String> = vec![];
+                for (k, _v) in irec {
+                    if i >= yaml_map.parsed_sample.len()
+                        || yaml_map.parsed_sample[i].get(k).is_none()
+                    {
+                        vo.push(k.clone());
+                    }
+                }
+                only_in_parse.push(vo);
+            }
+
+            for (i, irec) in yaml_map.parsed_sample.iter().enumerate() {
+                let mut vo: Vec<String> = vec![];
+                for (k, _v) in irec {
+                    if result[i].get(k).is_none() {
+                        vo.push(k.clone());
+                    }
+                }
+                only_in_yaml.push(vo);
+            }
+
+            println!("Only in yaml: {:?}", &only_in_yaml);
+            println!("Only in parse: {:?}", &only_in_parse);
+        }
     } else {
-        println!("Results differ");
-        println!("Records: {:?}", &result);
-        println!("\n");
-        println!("yaml: {:?}", &yaml_map.parsed_sample);
-        println!("\n");
-
-        let mut only_in_yaml: Vec<Vec<String>> = vec![];
-        let mut only_in_parse: Vec<Vec<String>> = vec![];
-
-        for (i, irec) in result.iter().enumerate() {
-            let mut vo: Vec<String> = vec![];
-            for (k, _v) in irec {
-                if i >= yaml_map.parsed_sample.len() || yaml_map.parsed_sample[i].get(k).is_none() {
-                    vo.push(k.clone());
-                }
-            }
-            only_in_parse.push(vo);
-        }
-
-        for (i, irec) in yaml_map.parsed_sample.iter().enumerate() {
-            let mut vo: Vec<String> = vec![];
-            for (k, _v) in irec {
-                if result[i].get(k).is_none() {
-                    vo.push(k.clone());
-                }
-            }
-            only_in_yaml.push(vo);
-        }
-
-        println!("Only in yaml: {:?}", &only_in_yaml);
-        println!("Only in parse: {:?}", &only_in_parse);
+        println!("Could not load YAML!");
     }
 }
